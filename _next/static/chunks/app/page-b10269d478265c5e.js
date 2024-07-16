bdfeae59cc74f35f055f66dd4d0000a195c70fdf
Jitendra <jitendra.keptbug@gmail.com>
2024-07-16 20:54:44 +0530

    delete old project

--- _next/static/chunks/app/page-b10269d478265c5e.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{3139:function(n,e,t){Promise.resolve().then(t.t.bind(t,231,23)),Promise.resolve().then(t.t.bind(t,3984,23))},3984:function(){}},function(n){n.O(0,[117,231,971,23,744],function(){return n(n.s=3139)}),_N_E=n.O()}]);
